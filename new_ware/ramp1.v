584e8f21022e9f1729b89ba90fc959997e518ab1
UB22.04Wang <2842wang6843w@gmail.com>
2026-01-26 09:24:59 +0800

    update 26-1-26

--- new_ware/ramp1.v
+++ new_ware/ramp1.v
@@ -3,23 +3,23 @@ module ramp1(
     input areset,
     output reg [3:0] q);
     
-    parameter S1 = 1, S2 = 2, S3 = 3, S4 = 4;
+    parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3;
     reg [1:0] state, next;
 
     always @(*) begin
         case(state) 
+            S0 : next = S1;
             S1 : next = S2;
-            S2 : next = S2;
-            S3 : next = S4;
-            S4 : next = S1;
-            default : next = S1;
+            S2 : next = S3;
+            S3 : next = S0;
+            default : next = S0;
         endcase
     end
 
 
     always @(posedge clk) begin
         if(areset) begin
-            state <= S1;
+            state <= S0;
         end
         else begin
             state <= next;
@@ -28,10 +28,10 @@ module ramp1(
 
     always @(*) begin
         case(state) 
-            S1: q = 4'b0101;
-            S2: q = 4'b1100;
-            S3: q = 4'b1001;
-            S4: q = 4'b0011;
+            S0: q = 4'b0101;
+            S1: q = 4'b1100;
+            S2: q = 4'b1001;
+            S3: q = 4'b0011;
             default: q = 4'b0000;
         endcase
     end
